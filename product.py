23c9858efe233d2c83bfebc742c4d14e5eb3a7a3
Tina <tina19960712520@gmail.com>
2023-01-12 15:48:59 +0800

    add product.py

---
+++ product.py
@@ -0,0 +1,11 @@
+products = []
+while True:
+    name = input('請輸入商品名稱:')
+    if name == 'q':
+        break
+    price = input('請輸入商品價格:') #因為若沒輸入品名,就不須問價格,所以寫在break之後
+    priducts.append([name,price])  #上面這行可以寫成這樣
+print(products)
+
+products[0][0] #products第0格清單中的 再第0格 (如何存取二維清單)
+
